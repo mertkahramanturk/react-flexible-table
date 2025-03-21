8b194c9add6a0592e6c156eb6b6e5937792a6134
mert.kahramanturk@parayla.com <mert.kahramanturk@parayla.com>
2025-03-21 09:54:48 +0300

    Updates

--- src/components/Spinner/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Spinner = () => {
-  return (
-		<div className="loaderSpinner"></div>
-  );
-};
-
-export default Spinner;
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import './index.css'; 
-
-export { default as FlexibleTable } from './Table';
